357b805550e820db5ae0fac6f9c8e9907dcb0b6a
Mark Rowe <mrowe@bdash.net.nz>
2025-04-27 22:22:28 -0700

    Crate to share my helper wrappers around Binary Ninja's Rust API

---
+++ README.md
@@ -0,0 +1,3 @@
+# bn-bdash-extras
+
+An assortment of helpers that I've found to be useful when writing Binary Ninja plug-ins in Rust.
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! An assortment of helpers that I've found to be useful when writing
+//! Binary Ninja plug-ins in Rust.
+
+pub mod activity;
+pub mod llil;
